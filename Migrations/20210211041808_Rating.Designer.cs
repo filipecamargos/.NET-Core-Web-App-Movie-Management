9ca389d5037f78dafe92c4b4334d0c12050d26f0
filipecamargos <filipe.atrabalho@gmail.com>
2021-02-10 21:20:44 -0700

    Rating added

--- Migrations/20210211041808_Rating.Designer.cs
+++ Migrations/20210211041808_Rating.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RazorPageMovieDataManager.Data;
 
 namespace RazorPageMovieDataManager.Migrations
 {
     [DbContext(typeof(RazorPageMovieDataManagerContext))]
-    partial class RazorPageMovieDataManagerContextModelSnapshot : ModelSnapshot
+    [Migration("20210211041808_Rating")]
+    partial class Rating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
