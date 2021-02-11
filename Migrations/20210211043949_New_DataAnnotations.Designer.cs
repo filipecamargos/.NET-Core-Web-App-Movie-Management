4df8f82b973872f47adb511991a95b7fe738a7dc
filipecamargos <filipe.atrabalho@gmail.com>
2021-02-10 21:48:23 -0700

    Assigment Completed

--- Migrations/20210211043949_New_DataAnnotations.Designer.cs
+++ Migrations/20210211043949_New_DataAnnotations.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RazorPageMovieDataManager.Data;
 
 namespace RazorPageMovieDataManager.Migrations
 {
     [DbContext(typeof(RazorPageMovieDataManagerContext))]
-    partial class RazorPageMovieDataManagerContextModelSnapshot : ModelSnapshot
+    [Migration("20210211043949_New_DataAnnotations")]
+    partial class New_DataAnnotations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
